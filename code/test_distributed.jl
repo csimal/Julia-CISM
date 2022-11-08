0fc65600796d61d0bab0d336dabece4e4b34e228
csimal <cedric.simal@unamur.be>
2022-11-08 23:49:37 +0100

    Add cluster example code

---
+++ code/test_distributed.jl
@@ -0,0 +1,17 @@
+using Distributed
+
+addprocs(4)
+
+println("Number of processes: ", nprocs())
+println("Number of workers: ", nworkers())
+
+# each worker gets its id, process id and hostname
+for i in workers()
+    id, pid, host = fetch(@spawnat i (myid(), getpid(), gethostname()))
+    println(id, " " , pid, " ", host)
+end
+
+# remove the workers
+for i in workers()
+    rmprocs(i)
+end
